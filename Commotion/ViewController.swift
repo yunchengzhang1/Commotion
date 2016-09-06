b0529f08f665d215bc419dd1970579cd13770709
Eric Larson <eclarson@erics-macbook-pro.local>
2016-09-06 12:41:59 -0500

    finished basic UI updates with activity

--- Commotion/ViewController.swift
+++ Commotion/ViewController.swift
@@ -14,11 +14,18 @@ class ViewController: UIViewController {
     // MARK: class variables
     let activityManager = CMMotionActivityManager()
     let pedometer = CMPedometer()
-
+    
+    //MARK: UI Elements
+    @IBOutlet weak var stepsSlider: UISlider!
+    @IBOutlet weak var stepsLabel: UILabel!
+    @IBOutlet weak var isWalking: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
+        self.stepsLabel.text = "No Steps Data"
         self.startActivityMonitoring()
         self.startPedometerMonitoring()
     }
@@ -28,28 +35,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Activity Functions
     func startActivityMonitoring(){
         // is activity is available
         if CMMotionActivityManager.isActivityAvailable(){
             // update from this queue (should we use the MAIN queue here??.... )
             self.activityManager.startActivityUpdatesToQueue(NSOperationQueue.mainQueue())
             {(activity:CMMotionActivity?)->Void in
-                // unwrap the activity and dispaly
+                // unwrap the activity and disp
                 if let unwrappedActivity = activity {
-                    print("%@",unwrappedActivity.description)
+                    dispatch_async(dispatch_get_main_queue()){
+                        self.isWalking.text = "Walking: \(unwrappedActivity.walking)\n Still: \(unwrappedActivity.stationary)"
+                    }
                 }
             }
         }
         
     }
     
+    // MARK: Pedometer Functions
     func startPedometerMonitoring(){
+        
+        //separate out the handler for better readability
         if CMPedometer.isStepCountingAvailable(){
-            pedometer.startPedometerUpdatesFromDate(NSDate())
-            {(pedData:CMPedometerData?, error:NSError?)->Void in
-                if pedData != nil {
-                    print("\(pedData!.description)")
-                }
+            pedometer.startPedometerUpdatesFromDate(NSDate(), withHandler: self.handlePedometer)
+        }
+    }
+    
+    //ped handler
+    func handlePedometer(pedData:CMPedometerData?, error:NSError?){
+        if pedData != nil {
+            let steps = pedData?.numberOfSteps
+            dispatch_async(dispatch_get_main_queue()){
+                self.stepsSlider.setValue((steps?.floatValue)!, animated: true)
+                self.stepsLabel.text = "Steps: \(steps!)"
             }
         }
     }
